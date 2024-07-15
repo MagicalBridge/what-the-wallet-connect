bae66f39dded3ee2ca18c629cb33a800a3964231
chupengfei <450793585@qq.com>
2024-07-16 07:12:05 +0800

    feat: add wagmi version

--- config/index.tsx
+++ config/index.tsx
@@ -1,30 +1,28 @@
+import { http, cookieStorage, createConfig, createStorage } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 
-// config/index.tsx
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-
-import { cookieStorage, createStorage } from 'wagmi'
-import { mainnet, sepolia,arbitrum } from 'wagmi/chains'
-
-// Your WalletConnect Cloud project ID
-export const projectId = '08cafd1223f468e1752d470a8cc08523'
-
-// Create a metadata object
-const metadata = {
-  name: 'louis web3 project',
-  description: 'AppKit Example',
-  url: 'https://web3modal.com', // origin must match your domain & subdomain
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
+export function getConfig() {
+  return createConfig({
+    chains: [mainnet, sepolia],
+    connectors: [
+      injected(),
+      coinbaseWallet(),
+      walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
+    ],
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+    ssr: true,
+    transports: {
+      [mainnet.id]: http(),
+      [sepolia.id]: http(),
+    },
+  })
 }
 
-// Create wagmiConfig
-const chains = [mainnet, sepolia, arbitrum] as const
-
-export const config = defaultWagmiConfig({
-  chains,
-  projectId,
-  metadata,
-  ssr: true,
-  storage: createStorage({
-    storage: cookieStorage
-  })
-})
+declare module 'wagmi' {
+  interface Register {
+    config: ReturnType<typeof getConfig>
+  }
+}
--- context/index.tsx
+++ context/index.tsx
@@ -1,37 +1,23 @@
 'use client'
 
-import React, { ReactNode } from 'react'
-import { config, projectId } from '@/config'
-
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { type ReactNode, useState } from 'react'
+import { type State, WagmiProvider } from 'wagmi'
 
-import { State, WagmiProvider } from 'wagmi'
+import { getConfig } from '@/config'
 
-// Setup queryClient
-const queryClient = new QueryClient()
-
-if (!projectId) throw new Error('Project ID is not defined')
-
-// Create modal
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
-
-export default function Web3ModalProvider({
-  children,
-  initialState
-}: {
+export function Providers(props: {
   children: ReactNode
   initialState?: State
 }) {
+  const [config] = useState(() => getConfig())
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <WagmiProvider config={config} initialState={props.initialState}>
+      <QueryClientProvider client={queryClient}>
+        {props.children}
+      </QueryClientProvider>
     </WagmiProvider>
   )
 }
